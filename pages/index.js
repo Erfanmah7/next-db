1fbe1a8bedac22e59576cdab51c0d196e8404b6f
Erfanmah7 <96947078+Erfanmah7@users.noreply.github.com>
2024-12-17 16:28:47 +0330

    post api

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,27 @@
+import { useState } from "react";
+
 export default function Home() {
-  return <>DB</>;
+  const [name, setName] = useState("");
+
+  const postHandler = async () => {
+    const res = await fetch("api/data", {
+      method: "POST",
+      body: JSON.stringify({ name }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
+  return (
+    <>
+      <h1>DB</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={postHandler}>send</button>
+    </>
+  );
 }
