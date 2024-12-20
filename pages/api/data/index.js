f2b72fb8618103c46d5fe489ae8b17678a9a70d1
Erfanmah7 <96947078+Erfanmah7@users.noreply.github.com>
2024-12-21 01:18:16 +0330

    handel errors

--- pages/api/data/index.js
+++ pages/api/data/index.js
@@ -2,23 +2,37 @@ import User from "@/models/User";
 import connectDB from "@/utils/connectDB";
 
 export default async function handler(req, res) {
-  //connect to DB
-  await connectDB();
+  try {
+    //connect to DB
+    await connectDB();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: "failed",
+      message: "error in connecting data in database",
+    });
+  }
 
   if (req.method === "POST") {
     const { name } = req.body;
 
-    if (!name || name.length <= 3) {
+    if (!name || name.length < 3) {
       res.status(422).json({ status: "failed", message: "invalid data" });
     }
 
-    // const user = new User({ name });
-    // await user.save();
-
-    const user = await User.create({ name });
-
-    res
-      .status(201)
-      .json({ status: "success", message: "created data", data: { name } });
+    try {
+      // const user = new User({ name });
+      // await user.save();
+      const user = await User.create({ name });
+      res
+        .status(201)
+        .json({ status: "success", message: "created data", data: user });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        status: "failed",
+        message: "error in storing data in database",
+      });
+    }
   }
 }
--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
 export default async function connectDB() {
-  try {
-    if (mongoose.connections[0].readyState) return;
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to DB");
-  } catch (error) {
-    console.error("Error connecting to DB", error);
-  }
+  if (mongoose.connections[0].readyState) return;
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("Connected to DB");
 }
