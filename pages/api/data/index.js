5db18006139b2eb2978760f9294407287eb7db80
Erfanmah7 <96947078+Erfanmah7@users.noreply.github.com>
2024-12-21 03:14:03 +0330

    get all data in DB

--- pages/api/data/index.js
+++ pages/api/data/index.js
@@ -11,6 +11,7 @@ export default async function handler(req, res) {
       status: "failed",
       message: "error in connecting data in database",
     });
+    return;
   }
 
   if (req.method === "POST") {
@@ -24,12 +25,12 @@ export default async function handler(req, res) {
       // const user = new User({ name });
       // await user.save();
       const user = await User.create({
-        name: "erfan",
-        age: 24,
-        phone: "0913000000",
-        email: "erfan@gmail.com",
-        address: { city: "ardakan", street: "abolfazl" },
-        courses:["next","react"]
+        name: "mohammad",
+        age: 27,
+        phone: "0912000000",
+        email: "mohammad@gmail.com",
+        address: { city: "ardakan", street: "hafez" },
+        courses: ["next"],
       });
       res
         .status(201)
@@ -39,6 +40,17 @@ export default async function handler(req, res) {
       res.status(500).json({
         status: "failed",
         message: "error in storing data in database",
+      });
+    }
+  } else if (req.method === "GET") {
+    try {
+      const users = await User.find();
+      res.status(200).json({ status: "success", data: users });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        status: "failed",
+        message: "error in retreving data in database",
       });
     }
   }
